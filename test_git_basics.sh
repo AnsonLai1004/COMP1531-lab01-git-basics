3abff736fedce1401a7f211664e28fe48fe9daa9
Khiet Tam Nguyen <nktnet999@gmail.com>
2022-05-28 12:10:38 +1000

    updated basics test

--- test_git_basics.sh
+++ test_git_basics.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-commits=`git rev-list HEAD --count`
+author="$(git config user.name)"
 
-if [ "$commits" -eq 1 ]
+if [ "$author" = "" ]
+then
+    echo 'You did not configure your user.name :(.'
+    exit 1
+fi
+
+if [ "$(git config user.email)" = "" ]
+then
+    echo 'You did not configure your user.email :(.'
+    exit 1
+fi
+
+commits=$(git rev-list HEAD --count --author="$author")
+
+if [ "$commits" -eq 0 ]
 then
     echo 'You did not make a commit :(.'
     exit 1
